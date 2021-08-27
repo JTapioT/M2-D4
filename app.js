084a20be2369b5a1bce1562e01070274e69d1a99
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-08-27 19:24:17 +0200

    Small changes to exercise

--- app.js
+++ app.js
@@ -1,38 +1,94 @@
 generateTeams = function() {
-  let generateTeamsBtn = document.querySelector(".generateTeamsBtn");
+  let generateTeamsBtn = document.getElementById("generateTeamsBtn");
   let textArea = document.querySelector(".form-control");
   let teamInputAmount = document.querySelector("#inputGroupSelect");
+
+  /* if(textArea.value === "") {
+    return; 
+    // Maybe try to introduce modal later which asks to name players 
+    // before generating teams.
+  } */
 
   // Generate teams button logic:
   generateTeamsBtn.onclick = function(event) {
     // Comes in one string form:
     let playersTotalStr = textArea.value;
-    // To array form:
+    // To Array form:
     let playersTotal = playersTotalStr.split(',');
 
     console.log("Total amount of players:");
     console.log(playersTotal.length);
 
-    // Calculations - Total teams and max players per team:
-    let teamsTotal = teamInputAmount.value;
-    let teamMaxPlayers = Math.ceil(playersTotal.length / teamsTotal);
+    // SMALL CALCULATIONS:
+    // Total amount of teams:
+    let teamsTotal = parseInt(teamInputAmount.value);
+    // Amount of players / team:
+    // Un-even teams taken into consideration?
+    let teamMaxPlayers;
+    let isNonEvenTeams;
+    if(playersTotal.length % 2 === 0) {
+      teamMaxPlayers = playersTotal.length / teamsTotal;
+    } else {
+      // In a way this creates a problem when later assigning players to teams?
+      isNonEvenTeams = true;
+      teamMaxPlayers = Math.floor(playersTotal.length / teamsTotal);
+    }
+    //teamMaxPlayers = playersTotal.length / teamsTotal;
 
-    // Teams - Array of sub-arrays which contain teams with players:
+    // Teams: Array of sub-arrays (teams) which contain players of the team:
     let teams = [];
-    for(let i=0; i<teamsTotal; i++) {
+    for(let i = 0; i<teamsTotal; i++) {
       teams.push([]);
     }
 
-    // Divide players to teams
-    for (let j = 0; j < playersTotal.length; j++) {
-      // NEEDS MORE LOGIC!!
 
-      for(let k=0; k < teams.length; k++) {
+    // Assign players to teams randomly
+    // Outer-loop - Go through each player
+    // Inner-loop - Assign randomly to available teams
+    for (let i = 0; i < playersTotal.length; i++) {
+
+      // In a situation when we have un-even teams. 
+      // Inner-loop below will assign players to teams until they are filled
+      // Hence, last player will always be left out of even teams
+      // Randomly just add last player to some team:
+      if(isNonEvenTeams && i === (playersTotal[i].length -1)) {
         let randomTeam = Math.floor(Math.random() * teams.length);
+        teams[randomTeam].push(playersTotal[i]);
+        break;
+      }
+
+      
+      
+   /*    let isTeamsEvenlyAssigned;
+      for (m = 0; m < teams.length; m++) {
+        if (m === teams.length - 1) {
+          if (teams[m].length === teams[m - 1].length) {
+            isTeamsEvenlyAssigned = true;
+            break;
+          } else {
+            isTeamsEvenlyAssigned = false;
+            break;
+          }
+        } else {
+          if (teams[m].length === teams[m + 1].length) {
+            isTeamsEvenlyAssigned = true;
+            continue;
+          } else {
+            isTeamsEvenlyAssigned = false;
+            continue;
+          }
+        }
+      } */
+
+      for(let k = 0;; k++) {
+        let randomTeam = Math.floor(Math.random() * teams.length);
+
         if(teams[randomTeam].length < teamMaxPlayers) {
-          teams[randomTeam].push(playersTotal[j]);
+          teams[randomTeam].push(playersTotal[i]);
           break;
-        } 
+        } else {
+          continue;
+        }
       }
     }
 
@@ -44,7 +100,7 @@ generateTeams = function() {
     // Create elements for teams:
     let containerElement = document.createElement('div');
     containerElement.classList.add('container');
-    containerElement.classList.add('teamContainer');
+    containerElement.id = 'teamContainer';
     document.body.appendChild(containerElement);
 
 
@@ -69,13 +125,18 @@ generateTeams = function() {
 
 
   // Reset button logic:
-  let resetTeamsBtn = document.querySelector(".resetTeamsBtn");
+  let resetTeamsBtn = document.getElementById("resetTeamsBtn");
+
   resetTeamsBtn.onclick = function() {
     textArea.value = "";
     teamInputAmount.value = "Choose amount of teams";
 
-    let teamContainer = document.querySelector(".teamContainer");
-    teamContainer.remove();
+    let teamContainer = document.getElementById("teamContainer");
+    if(teamContainer !== null) {
+      teamContainer.remove();
+    } else {
+      return;
+    }
   }
 
 }
